3ce9d27ba63f5d3433bfdb61df6f9ce5c3c54424
lonys <lonys@LAPTOP-8GOJV76L>
2023-12-04 10:31:40 +0100

    Add project files.

---
+++ PyramidOfNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("***");
+Console.WriteLine("Pyramid of numbers");
+Console.WriteLine("***");
+Console.WriteLine("Enter your number");
+
+int a = 0;
+int b = 1;
+
+a = Int32.Parse(Console.ReadLine());
+
+for (int i = 0; i < a; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+       
+        {
+            b++;
+        }
+        Console.WriteLine(b);
+    }
+}
+
+        
+
+    
+    
+
